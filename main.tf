7032e91f10de4f8535508ede8328455fc5ffdcae
vince87-87 <vince87.87@gmail.com>
2021-07-11 14:57:53 +0800

    create variables,main.tf with source code

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
+}
+
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+# Create public subnets
+resource "aws_subnet" "public" {
+count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+vpc_id                     = aws_vpc.main.id
+cidr_block                 = cidrsubnet(var.vpc_cidr, 8 , count.index+1)
+map_public_ip_on_launch    = true
+availability_zone          = data.aws_availability_zones.available.names[count.index]
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+    default = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+      default = 2
+}
+
+provider "aws" {
+    region = var.region
+}
